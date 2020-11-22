93e63c2d64650a77d82cf4ab2298e307c090faeb
thenatural86 <philip.smalls86@gmail.com>
2020-11-22 14:45:36 -0500

    selenium

---
+++ selenium/tests.py
@@ -0,0 +1,12 @@
+import os
+import pathlib
+import unittest
+
+from selenium import webdriver
+
+
+def file_uri(filename):
+    return pathlib.Path(os.path.abspath(filename)).as_uri()
+
+
+driver = webdriver.Chrome()
